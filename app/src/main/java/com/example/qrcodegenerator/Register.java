07c7c25ed49a4e1528e8f4be68c32d9d4a1346ed
Kkaguamba <kkaguamba@gmail.com>
2024-02-15 18:30:36 +0300

    QRCode Gen

--- app/src/main/java/com/example/qrcodegenerator/Register.java
+++ app/src/main/java/com/example/qrcodegenerator/Register.java
@@ -8,17 +8,27 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
+    EditText uname,email,phone,pass,conf_pass;
     Button registerbutton;
     TextInputLayout layoutPassword;
     TextInputEditText txtPassword;
+    FirebaseDatabase db;
+    DatabaseReference ref;
+    User user;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,17 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         layoutPassword = findViewById(R.id.layoutpass);
         txtPassword = findViewById(R.id.txt_pass);
+        uname = findViewById(R.id.txt_name);
+        email = findViewById(R.id.txt_email);
+        phone = findViewById(R.id.txt_phone);
+        pass = findViewById(R.id.txt_pass);
+        conf_pass = findViewById(R.id.txt_confpass);
+        registerbutton = findViewById(R.id.register_btn);
+
+        //write to database
+        db = FirebaseDatabase.getInstance();
+        ref = db.getReference("user info");
+        user = new User();
 
         //input validation
         txtPassword.addTextChangedListener(new TextWatcher() {
@@ -61,12 +82,27 @@ public class Register extends AppCompatActivity {
 
             }
         });
-        registerbutton = findViewById(R.id.register_btn);
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
+                String Uname = uname.getText().toString();
+                String Email = email.getText().toString();
+                String Phone = phone.getText().toString();
+                String Passwd = pass.getText().toString();
+                String Conf_passwd = conf_pass.getText().toString();
+
+                if (Uname.isEmpty() || Email.isEmpty() || Phone.isEmpty() || Passwd.isEmpty() ||
+                        Conf_passwd.isEmpty()) {
+                    Toast.makeText(Register.this, "Please fill all the fields",
+                            Toast.LENGTH_SHORT).show();
+                } else if (Passwd.equals(Conf_passwd)) {
+                    Intent i = new Intent(getApplicationContext(), Login.class);
+                    startActivity(i);
+
+                } else {
+                    Toast.makeText(Register.this, "Password do not match!",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
